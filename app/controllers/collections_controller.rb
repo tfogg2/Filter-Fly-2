b86ad4a5c3bd931bb5dc0940b142424c4ce56b84
tfogg2 <foggts@g.cofc.edu>
2017-01-05 17:46:17 -0500

    second store

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -2,14 +2,12 @@ class CollectionsController < ShopifyApp::AuthenticatedController
 	before_action :clear_shopify_collection_id, :only => :index
 	
 	def index
-		Rails.logger.debug("set_shopify_collection_id: #{session[:shopify_collection_id]}")
+		# Rails.logger.debug("set_shopify_collection_id: #{session[:shopify_collection_id]}")
 
 		@products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-		@customCollections = ShopifyAPI::CustomCollection.find(:all, params: { limit: 10 })
-		@smartCollections = ShopifyAPI::SmartCollection.find(:all, params: { limit: 10 })
+		@customCollections = ShopifyAPI::CustomCollection.find(:all, params: { limit: 10 }).page(params[:page]).per(10)
+		@smartCollections = ShopifyAPI::SmartCollection.find(:all, params: { limit: 10 }).page(params[:page]).per(10)
 		@categories = Category.where(shopify_collection_id: session[:shopify_collection_id])
 
-		@customCollections = CustomCollection.page(params[:page]).per(10)
-		@smartCollections = SmartCollection.page(params[:page]).per(10)
 	end
 end
