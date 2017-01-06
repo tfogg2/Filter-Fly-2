21efbeb83357935a9f5d9d0128cab3f7864b231e
tfogg2 <foggts@g.cofc.edu>
2017-01-06 16:08:14 -0500

    home load

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -132,6 +132,15 @@ $(document).ready(function () {
 
 
 
+	$('#home').on("load", function(){
+		 window.setTimeout(function(){
+
+        	// Move to a new location or you can do something else
+        	window.location.href = "https://filterfly2.herokuapp.com/index";
+
+    	}, 5000);
+	});
+
 
 
 
