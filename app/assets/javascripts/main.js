70103799459fd24d82fff21d2abf160d436a8078
tfogg2 <foggts@g.cofc.edu>
2017-01-19 16:04:15 -0500

    data table

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -173,6 +173,22 @@ $(document).ready(function () {
 
 
 
+
+	// TEST DATA TABLE //
+	// '.tbl-content' consumed little space for vertical scrollbar, scrollbar width depend on browser/os/platfrom. Here calculate the scollbar width .
+		$(window).on("load resize ", function() {
+		  var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+		  $('.tbl-header').css({'padding-right':scrollWidth});
+		}).resize();
+
+
+
+
+
+
+	// TEST DATA TABLE //
+
+
 });
 
 
