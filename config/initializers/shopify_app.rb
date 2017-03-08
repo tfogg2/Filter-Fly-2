7a79273ae10435720d21bfff27e22c93c56777ad
tfogg2 <foggts@g.cofc.edu>
2017-03-08 17:26:28 -0500

    random js file

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,10 +2,10 @@ ShopifyApp.configure do |config|
 
   config.api_key = "ef2b4e3a8c09d66e0eece4c732b271fb"
   config.secret = "d866681c45bd81e34296d70d6f9d786b"
-  config.scope = "read_orders, read_products"
+  config.scope = "read_orders, read_products, read_script_tags, write_script_tags"
   config.embedded_app = true
   config.scripttags = [{
 	event:'onload',
-	src: 'https://connect.facebook.net/en_US/fbevents.js'
+	src: 'https://filterfly2.herokuapp.com/main.js'
   }]
 end
