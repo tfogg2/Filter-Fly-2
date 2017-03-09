b2cf9d95013a1c8329e1b396c94f40bde4c87bd8
tfogg2 <foggts@g.cofc.edu>
2017-03-09 10:46:17 -0500

    children

--- public/main.js
+++ public/main.js
@@ -6,6 +6,7 @@
 
 var filters = document.getElementById("filters");
 $(filters).css("min-height", "150px");
-$(filters).css("width", "100px");
+$(filters).css("width", "150px");
 var ul = $(filters).children("ul");
 $(ul).addClass("collections");
+$(filters).replaceWith("<%= j render('shared/filters', {product: @product, category: @category, product_type: @product_type, tags: @tags}) %>"))
