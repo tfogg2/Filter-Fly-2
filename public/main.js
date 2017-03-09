37161e472077429a699b3949512d560ce364428f
tfogg2 <foggts@g.cofc.edu>
2017-03-08 22:35:51 -0500

    public

--- public/main.js
+++ public/main.js
@@ -1,5 +1,8 @@
-alert("We made it!");
-console.log(6*12);
-var testJava = document.getElementById("testJava");
-$(testJava).addClass("red");
-console.log("boom");
+// alert("We made it!");
+// console.log(6*12);
+// var testJava = document.getElementById("testJava");
+// $(testJava).addClass("red");
+// console.log("boom");
+
+var filters = document.getElementById("filters");
+$(filters).add("ul").css("background", "red");
