f64f183d73d33dbef17cb71530e7bc2c1862b57b
tfogg2 <foggts@g.cofc.edu>
2017-03-10 11:44:18 -0500

    ajax

--- public/main.js
+++ public/main.js
@@ -29,27 +29,3 @@ function getProducts(){
 
 
 
-// var ff_shop_id;
-// var domain =  shop.id;
-// console.log(myshopify_domain);
-
-// function ff_init(shop_id) {
-// 	ff_shop_id = shop_id;
-// 	getProducts();
-// }
-
-
-// function getProducts(){
-// 	if(!ff_shop_id) {
-// 		console.log("No shop id set");
-// 		return
-// 	}
-
-// 	// Go get products for ff_shop_id
-// }
-
-
-// ff_init({my_shop_id})
-
-// 
-
