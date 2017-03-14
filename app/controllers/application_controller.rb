f63171bedd37dab7c1ddb61a56afa0fd802a3752
tfogg2 <foggts@g.cofc.edu>
2017-03-14 17:01:32 -0400

    shop session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,9 +28,7 @@ class ApplicationController < ActionController::Base
     shop = ShopifyApp::SessionRepository.retrieve(shop_domain)
     #shop = self.find_or_initialize_by(shopify_domain: session.url)
     #ShopifyAPI::Base.activate_session(shop)
-    shop.shopify_token = session.token
-    shop.save!
-    shop.id
+
     #@shop = Shop.find_by_shopify_domain(params[:shop])
   end
   
