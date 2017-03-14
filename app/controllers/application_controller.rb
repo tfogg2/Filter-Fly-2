41e29272137a142c2ab76e7f31e6854ba82eede3
tfogg2 <foggts@g.cofc.edu>
2017-03-14 13:04:28 -0400

    shop session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :set_shop
   before_action :set_shopify_collection_id
-
   #before_action :logger
 
   
@@ -23,8 +23,11 @@ class ApplicationController < ActionController::Base
   end
 
   def set_shop
+    shop_domain = params[:shop]
     # This needs to look the shop up by it's store name? url?
-    # @shop = blah
+    shop = ShopifyApp::SessionRepository.retrieve(shop_domain)
+    ShopifyAPI::Base.activate_session(shop)
+    #@shop = Shop.find_by_shopify_domain(params[:shop])
   end
   
   #def logger
