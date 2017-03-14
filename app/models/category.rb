5b69b6f20d17f17ba96b613ff2da3314761b51c3
tfogg2 <foggts@g.cofc.edu>
2017-03-14 13:21:07 -0400

    shop session

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-	belongs_to :shopify_collection
+	belongs_to :collection
 	has_many :product_types, :dependent => :destroy
 	has_many :products, :dependent => :destroy
 	validates :title, presence: true
