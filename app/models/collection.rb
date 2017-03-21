754377362946ba0edf0625b76908380424d79df7
tfogg2 <foggts@g.cofc.edu>
2017-03-21 14:57:38 -0400

    collection store

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -2,6 +2,19 @@ class Collection < ApplicationRecord
 	belongs_to :shop
 	has_many :categories, dependent: :destroy
 
+	def find_or_create_by(collection)
+		# collection is response from shopify api
+		c = collections.find_by_shopify_collection_id(c.id)
+		return c if c
+
+		Rails.logger.debug("Collection: #{collection}")
+
+		collections.create(
+			title: collection[:title],
+			shopify_collection_id: collection.id #[:id], # Dont know what the actual id is called
+		)
+	end
+	
 	def as_json
 		{
 			id: id,
