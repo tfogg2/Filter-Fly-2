6fb85c3e07284edd97b1b188f56b83d1474556b2
tfogg2 <foggts@g.cofc.edu>
2017-03-21 12:55:19 -0400

    collection store

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -10,7 +10,7 @@ class CollectionsController < ShopifyApp::AuthenticatedController
 
 		# Create custom collections
 		@customCollections.each do |c|
-			@collection = CustomCollection.find_or_create_by(c_id: c.id )
+			@collection = @shop.find_or_create_by(c_id: c.id )
 			#Shop.find_or_create_collection(c)  #title: c.title, collection_id: c.id)
 		end
 
