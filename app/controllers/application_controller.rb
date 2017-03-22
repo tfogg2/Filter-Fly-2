45114995fa38978d198c00bd4d40cd704e6fe61d
tfogg2 <foggts@g.cofc.edu>
2017-03-22 12:22:21 -0400

    merge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,37 +2,32 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :set_shop
-  before_action :set_shopify_collection_id
+  # before_action :set_shopify_collection_id
   #before_action :logger
 
-  
 
-  def set_shopify_collection_id
-  	if !params[:shopify_collection_id].blank?
-  		session[:shopify_collection_id] = params[:shopify_collection_id]
-  	end
-  	
-  	if !params[:shopify_collection_title].blank?
-  		session[:shopify_collection_title] = params[:shopify_collection_title]
-  	end
-  end
 
-  def clear_shopify_collection_id
-  	session[:shopify_collection_id] = nil
-  	session[:shopify_collection_title] = nil
-  end
+  # def set_shopify_collection_id
+  # 	if !params[:shopify_collection_id].blank?
+  # 		session[:shopify_collection_id] = params[:shopify_collection_id]
+  # 	end
+  #
+  # 	if !params[:shopify_collection_title].blank?
+  # 		session[:shopify_collection_title] = params[:shopify_collection_title]
+  # 	end
+  # end
+
+  # def clear_shopify_collection_id
+  # 	session[:shopify_collection_id] = nil
+  # 	session[:shopify_collection_title] = nil
+  # end
 
   def set_shop
-    shop_domain = params[:shop]
-    # # This needs to look the shop up by it's store name? url?
-    shop = ShopifyApp::SessionRepository.retrieve(shop_domain)
-    Rails.logger.debug(shop)
-    #shop = self.find_or_initialize_by(shopify_domain: session.url)
-    #ShopifyAPI::Base.activate_session(shop)
-
-    #@shop = Shop.find_by_shopify_domain(params[:shop])
+    # shop = self.find_or_initialize_by(shopify_domain: session.url)
+    # ShopifyAPI::Base.activate_session(shop)
+    @shop = Shop.find_by_shopify_domain(params[:shop])
   end
-  
+
   #def logger
    # Rails.logger.debug("///set_shopify_collection_id: #{session[:shopify_collection_id]}")
   #end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,15 +4,16 @@ class Shop < ActiveRecord::Base
 
   has_many :collections, dependent: :destroy
 
-  	def find_or_create_by(collection)
-		# collection is response from shopify api
-		c = collections.find_by_shopify_collection_id(c.id)
+  	def find_or_create_collection(collection)
+		# collection is response from sohpify api
+		c = collections.find_by_shopify_collection_id(collection.id)
+    Rails.logger.debug("Found existing collection (#{collection.class.name}): #{c.title}") if c
 		return c if c
 
-		Rails.logger.debug("Collection: #{collection}")
+		Rails.logger.debug("No collection yet for this shopify collection: #{collection.inspect}")
 
 		collections.create(
-			title: collection[:title],
+			title: collection.title,
 			shopify_collection_id: collection.id #[:id], # Dont know what the actual id is called
 		)
 	end
