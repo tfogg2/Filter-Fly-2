f9a7b22523a3552bafd0b85ec3aa772fc7b27d4d
tfogg2 <foggts@g.cofc.edu>
2017-03-23 16:54:15 -0400

    set shop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,8 +26,9 @@ class ApplicationController < ActionController::Base
   # end
 
   def set_shop
-    @shop = Shop.find_by_shopify_domain(params[:shop])
-    ShopifyAPI::Session.new(@shop.domain, @shop.token)
+    #@shop = Shop.find_by_shopify_domain(params[:shop])
+    shop = Shop.find_by_shopify_domain(params[:shop])
+    ShopifyAPI::Session.new(shop.domain, shop.token)
   end
 
 
