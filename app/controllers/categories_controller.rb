7714d22f9c003fbd978ce19b6e09a2ea97560c6c
tfogg2 <foggts@g.cofc.edu>
2017-03-24 14:56:29 -0400

    application controller shopify

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -97,7 +97,7 @@ class CategoriesController <ApplicationController #ShopifyApp::AuthenticatedCont
       #current_shop = ShopifyAPI::Shop.current
       @shop = Shop.find_by_shopify_domain(params[:shop])
       #find_by_shopify_domain(current_shop.shopify_domain)
-      @collection = Collection.find(params[:shopify_collection_id] || params[:shopify_collection_id])
+      @collection = Collection.find(params[:shopify_collection_id] || params[:id])
     end
     # Use callbacks to share common setup or constraints between actions.
     def set_category
