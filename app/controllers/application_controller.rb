e326d5357697610f99d1a559658459b8e5665164
tfogg2 <foggts@g.cofc.edu>
2017-03-24 11:15:04 -0400

    set collection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < ActionController::Base
   def set_shop
     #@shop = Shop.find_by_shopify_domain(params[:shop])
     @shop = Shop.find_by_shopify_domain(params[:shop])
-
+    self.retrieve(id)
   end
 
 
