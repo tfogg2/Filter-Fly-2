61ba5ea2092ffde221319d9b828814053adf8247
tfogg2 <foggts@g.cofc.edu>
2017-03-25 15:14:45 -0400

    application controller shopify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < ActionController::Base
   def set_shop
     #@shop = Shop.find_by_shopify_domain(params[:shop])
     current_shop = ShopifyAPI::Shop.current
-    @shop = Shop.find_by_shopify_domain(current_shop.shopify_domain)
+    @shop = Shop.find_by_shopify_domain(params[:current_shop])
     #@shop = Shop.find_by_shopify_domain(params[:shop])
   end
 
