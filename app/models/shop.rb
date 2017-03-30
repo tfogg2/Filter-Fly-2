27039564cbc805f8509adf1c73a8d43bff9d5142
tfogg2 <foggts@g.cofc.edu>
2017-03-30 15:27:26 -0400

    domain

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -5,7 +5,7 @@ class Shop < ActiveRecord::Base
   has_many :collections, dependent: :destroy
 
   def self.store(session)
-   	shop = Shop.new(shopify_domain: session.url, shopify_token: session.shopify_token) #session.url
+   	shop = Shop.new(domain: session.url, token: session.shopify_token) #session.url
   	shop.save!
    	shop.id
   end
