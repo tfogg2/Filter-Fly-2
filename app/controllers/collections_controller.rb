0f771253c19d070d57957b88cc7429fe1cfa0025
tfogg2 <foggts@g.cofc.edu>
2017-08-11 12:20:53 -0400

    rake db

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -5,15 +5,15 @@ class CollectionsController < ShopifyApp::AuthenticatedController
 		@custom_collections = ShopifyAPI::CustomCollection.find(:all) #, params: { limit: 10 }
 		@smart_collections = ShopifyAPI::SmartCollection.find(:all) #, params: { limit: 10 }
 
-		# # Create custom collections
-		# @custom_collections.each do |c|
-		# 	@shop.find_or_create_collection(c)
-		# end
-		#
-		# # Create smart collections
-		# @smart_collections.each do |c|
-		# 	@shop.find_or_create_collection(c)
-		# end
+		# Create custom collections
+		@custom_collections.each do |c|
+			@shop.find_or_create_collection(c)
+		end
+
+		# Create smart collections
+		@smart_collections.each do |c|
+			@shop.find_or_create_collection(c)
+		end
 	end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170312231422) do
+ActiveRecord::Schema.define(version: 20170807225801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_admin_comments", id: :bigserial, force: :cascade do |t|
+    t.string   "namespace"
+    t.text     "body"
+    t.string   "resource_type"
+    t.integer  "resource_id"
+    t.string   "author_type"
+    t.integer  "author_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
+  end
+
+  create_table "activities", id: :bigserial, force: :cascade do |t|
+    t.string   "description"
+    t.string   "title"
+    t.string   "location"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "event_id"
+    t.datetime "date"
+    t.datetime "time"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -30,6 +55,16 @@ ActiveRecord::Schema.define(version: 20170312231422) do
     t.string   "shopify_collection_id"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
+  end
+
+  create_table "events", id: :bigserial, force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "location"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.datetime "start_date"
+    t.datetime "end_date"
   end
 
   create_table "product_tags", force: :cascade do |t|
@@ -68,6 +103,16 @@ ActiveRecord::Schema.define(version: 20170312231422) do
     t.index ["shopify_domain"], name: "index_shops_on_shopify_domain", unique: true, using: :btree
   end
 
+  create_table "supplementals", id: :bigserial, force: :cascade do |t|
+    t.string   "attachment_file_name"
+    t.string   "attachment_content_type"
+    t.integer  "attachment_file_size"
+    t.datetime "attachment_updated_at"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.string   "event_id"
+  end
+
   create_table "tags", force: :cascade do |t|
     t.integer  "product_type_id"
     t.string   "title"
@@ -75,6 +120,35 @@ ActiveRecord::Schema.define(version: 20170312231422) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.index ["product_type_id"], name: "index_tags_on_product_type_id", using: :btree
+  end
+
+  create_table "teams", id: :bigserial, force: :cascade do |t|
+    t.integer  "car_number"
+    t.string   "competitor_type"
+    t.string   "car_type"
+    t.string   "driver_name"
+    t.string   "co_driver_name"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "car_class"
+  end
+
+  create_table "users", id: :bigserial, force: :cascade do |t|
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "admin",                  default: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "product_tags", "products"
