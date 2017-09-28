cc9beb3919561ecd081532794adc47159c026168
tfogg2 <foggts@g.cofc.edu>
2017-09-28 16:34:45 -0400

    search

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -2,12 +2,12 @@ class CollectionsController < ShopifyApp::AuthenticatedController
 	# before_action :set_shop
 	def index
 		@products = ShopifyAPI::Product.find(:all) #, params: { limit: 10 }
-		@custom_collections = ShopifyAPI::CustomCollection.search(params[:search]) #.find(:all) #, params: { limit: 10 }
-		@smart_collections = ShopifyAPI::SmartCollection.search(params[:search]) #.find(:all) #, params: { limit: 10 }
+		@custom_collections = ShopifyAPI::CustomCollection.find(:all) #, params: { limit: 10 }
+		@smart_collections = ShopifyAPI::SmartCollection.find(:all) #, params: { limit: 10 }
 
-		@collections = @custom_collections && @smart_collections
+		@collections = (@custom_collections && @smart_collections).search(params[:search])
 
-		@collections_search = @collections.search(params[:search])
+		# @collections_search = @collections.search(params[:search])
 		# Create custom collections
 		@custom_collections.each do |c|
 			@shop.find_or_create_collection(c)
