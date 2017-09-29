6dc0529d689e6f21b535e8daed10cc8a1d5865f0
tfogg2 <foggts@g.cofc.edu>
2017-09-29 17:48:04 -0400

    categories

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -176,14 +176,14 @@ $(document).ready(function () {
 
 	$('.id').on('click', function(){
 		var collection_id = $(this).data('collection-id');
-		$.ajax({
-			type: 'GET',
-			url: 'collections/' + collection_id + '/categories',
-			success: function(data){
-				$('.conversation-header').append(data);
-			}
-		});
-		return false;
+		// $.ajax({
+		// 	type: 'GET',
+		// 	url: 'collections/' + collection_id + '/categories',
+		// 	success: function(data){
+		// 		$('.conversation-header').append(data);
+		// 	}
+		// });
+		// return false;
 
 		// $getScript('categories/form.js.erb', function(script){
 		//
