941ab72be21a40ff3f126b7e1dc4ca5926cbb0fc
tfogg2 <foggts@g.cofc.edu>
2017-09-29 16:09:19 -0400

    categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -34,7 +34,7 @@ class CategoriesController < ShopifyApp::AuthenticatedController
   # GET /categories/new
   def new
     # @category = @collection.category.new(@collection)
-    @collection = Collection.find(params[:id])
+    @collection = Collection.find(shopify_collection_id: session[:shopify_collection_id])
     @category = @collection.categories.new()
     # @category = Category.new(shopify_collection_id: session[:shopify_collection_id])
   end
