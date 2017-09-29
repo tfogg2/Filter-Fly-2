e5a4c45c2e3c9e71f03aebb1e6728914709f8cdf
tfogg2 <foggts@g.cofc.edu>
2017-09-29 16:06:31 -0400

    categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170723171011) do
+ActiveRecord::Schema.define(version: 20170718222949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,38 @@ ActiveRecord::Schema.define(version: 20170723171011) do
     t.string   "shopify_collection_id"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
+  end
+
+  create_table "conversations", id: :bigserial, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "project_id"
+  end
+
+  create_table "messages", id: :bigserial, force: :cascade do |t|
+    t.text     "body"
+    t.bigint   "conversation_id"
+    t.bigint   "user_id"
+    t.boolean  "read",               default: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
+  end
+
+  create_table "notifications", id: :bigserial, force: :cascade do |t|
+    t.bigint   "user_id"
+    t.integer  "recipient_id"
+    t.string   "action"
+    t.string   "notifiable_type"
+    t.integer  "notifiable_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["user_id"], name: "index_notifications_on_user_id", using: :btree
   end
 
   create_table "product_tags", force: :cascade do |t|
@@ -130,6 +162,7 @@ ActiveRecord::Schema.define(version: 20170723171011) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "notifications", "users"
   add_foreign_key "product_tags", "products"
   add_foreign_key "product_tags", "tags"
   add_foreign_key "product_types", "categories"
